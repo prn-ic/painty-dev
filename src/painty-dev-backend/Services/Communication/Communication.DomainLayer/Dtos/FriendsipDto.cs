37c7b74c0df97c088e4452333aa9f0baa4390478
prnic <domarlaid2@mail.ru>
2023-10-18 03:09:22 +0500

    add dtos

---
+++ src/painty-dev-backend/Services/Communication/Communication.DomainLayer/Dtos/FriendsipDto.cs
@@ -0,0 +1,10 @@
+﻿using Communication.DomainLayer.Entities;
+
+namespace Communication.DomainLayer.Dtos
+{
+    public class FriendsipDto : EntityBase
+    {
+        public Guid? RequestFromId { get; set; }
+        public Guid? RequestToId { get; set; }
+    }
+}
---
+++ src/painty-dev-backend/Services/Communication/Communication.DomainLayer/Dtos/ImageDto.cs
@@ -0,0 +1,10 @@
+﻿using Communication.DomainLayer.Entities;
+
+namespace Communication.DomainLayer.Dtos
+{
+    public class ImageDto : EntityBase
+    {
+        public string? ImagePath { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
