e5f97e3d2b44a09244cffc5f4a42cbe9755eb90f
prnic <domarlaid2@mail.ru>
2023-10-18 04:47:06 +0500

    update Program.cs

--- src/painty-dev-backend/Services/Communication/Communication.Api/Program.cs
+++ src/painty-dev-backend/Services/Communication/Communication.Api/Program.cs
@@ -6,6 +6,7 @@ using Communication.DomainLayer.Contracts;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,41 @@ builder.Configuration.AddJsonFile("appsettings.json", true, true);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Add Swagger Security UI
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "Jwt",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+             new string[] {}
+          }
+        });
+});
 
 // Data layer
 builder.Services.AddDbContext<AppDbContext>(options =>
