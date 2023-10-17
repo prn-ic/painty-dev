ceed98f658ed3c58f548da5320f9c9126568fdec
prnic <domarlaid2@mail.ru>
2023-10-18 00:23:29 +0500

    add authentication service

--- src/painty-dev-backend/Services/Authentication/Authentication.BusinessLayer/Exceptions/ClientExceptions/InvalidDataException.cs
+++ src/painty-dev-backend/Services/Authentication/Authentication.BusinessLayer/Exceptions/ClientExceptions/InvalidDataException.cs
@@ -10,5 +10,11 @@ namespace Authentication.BusinessLayer.Exceptions
             : base((int)ClientErrorCodes.BadRequest, message) { }
         public InvalidDataException(T data, string message = "Invalid data") :
             base(CustomResponse.BadRequestResult(data, message)) { }
+        public InvalidDataException(T data, string[] parameters) :
+            base(CustomResponse.BadRequestResult(data, 
+                "Some parameters is incorrect: " + string.Join(";", parameters))) { }
+        public InvalidDataException(string[] parameters) :
+            base(CustomResponse.BadRequestResult(
+                "Some parameters is incorrect: " + string.Join(";", parameters))) { }
     }
 }
--- src/painty-dev-backend/Services/Authentication/Authentication.BusinessLayer/Services/AuthenticationService.cs
+++ src/painty-dev-backend/Services/Authentication/Authentication.BusinessLayer/Services/AuthenticationService.cs
@@ -1,24 +1,64 @@
 ﻿using Authentication.BusinessLayer.Contracts;
+using Authentication.BusinessLayer.Exceptions;
 using Authentication.BusinessLayer.Models;
+using Authentication.DomainLayer.Contracts;
 using Authentication.DomainLayer.Dtos;
+using Authentication.DomainLayer.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Authentication.BusinessLayer.Services
 {
     public class AuthenticationService : IAuthenticationService
     {
-        public Task<TokenModel> RefreshAsync(string token)
+        private readonly IUserService _userService;
+        private readonly IJwtService _jwtService;
+
+        public AuthenticationService(IUserService userService, IJwtService jwtService)
         {
-            throw new NotImplementedException();
+            _userService = userService;
+            _jwtService = jwtService;
+        }
+        public async Task<TokenModel> RefreshAsync(string token)
+        {
+            ClaimsPrincipal principal = _jwtService.GetClaimsPrincipal(token);
+            if (principal is null)
+                throw new InvalidTokenException<User>();
+
+            User? user= await _userService.GetByNameAsync(
+                principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)!.Value);
+
+            return _jwtService.CreateTokenPair(user!);
         }
 
-        public Task<TokenModel> SignInAsync(UserDto userDto)
+        public async Task<TokenModel> SignInAsync(UserDto userDto)
         {
-            throw new NotImplementedException();
+            User? user = await _userService.GetByNameAsync(userDto.Name!);
+
+            if (user is null)
+                throw new NotFoundException<User>();
+
+            if (!user.Validate(userDto.Password!))
+                throw new InvalidDataException<User>();
+
+            return _jwtService.CreateTokenPair(user);
         }
 
-        public Task<TokenModel> SignUpAsync(UserDto userDto)
+        public async Task<TokenModel> SignUpAsync(UserDto userDto)
         {
-            throw new NotImplementedException();
+            if (userDto.Name is null) throw new InvalidDataException<User>(new[] { "Name" });
+
+            if (userDto.Password is null) throw new InvalidDataException<User>(new[] { "Password" });
+
+            if (await _userService.GetByNameAsync(userDto.Name) is not null)
+                throw new ConflictException<User>();
+
+            User user = new User(userDto.Name, userDto.Password);
+            await _userService.CreateAsync(new User(userDto.Name, userDto.Password));
+
+            // Generate a confirmation token 
+            return _jwtService.CreateTokenPair(user);
         }
+
     }
 }
